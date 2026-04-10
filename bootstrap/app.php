b11e13984fbea31658e1623986fff0c9757a0584
Aws12505 <awsaljaramani12505@gmail.com>
2026-04-10 16:43:12 -0400

    ..

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -9,7 +9,6 @@ return Application::configure(basePath: dirname(__DIR__))
         web: __DIR__ . '/../routes/web.php',
         api: __DIR__ . '/../routes/api.php',
         commands: __DIR__ . '/../routes/console.php',
-        channels: __DIR__ . '/../routes/channels.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
@@ -18,8 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withBroadcasting(__DIR__ . '/../routes/channels.php', [
-        // Add your custom alias here
-        'middleware' => ['auth.token.store'],
+        'middleware' => ['web', 'auth.token.store'],
     ])
     ->withExceptions(function (Exceptions $exceptions): void {
         //
