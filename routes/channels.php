ea8b4643e7cdd7a115fc9d2b5860283606ffcf99
Aws12505 <awsaljaramani12505@gmail.com>
2026-04-10 16:47:36 -0400

    ..

--- routes/channels.php
+++ routes/channels.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Broadcast;
+use Illuminate\Support\Facades\Log;
 
 Broadcast::channel('users.{userId}', function ($user, $userId) {
+    Log::info('channel callback hit', [
+        'auth_user_id' => $user?->id,
+        'requested_user_id' => $userId,
+    ]);
+
     return (int) $user->id === (int) $userId;
 });
